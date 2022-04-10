2117a176c5178e7dde511aff2de92b1dbae58b45
ADITYA SINGH <adityasingh10042@gmail.com>
2022-04-10 16:34:27 +0530

    qa : More Assets added

---
+++ Assets/scripts/ChooseMenu.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ChooseMenu : MonoBehaviour
+{
+    public void DanceOptions()
+    {
+        SceneManager.LoadScene(4);
+    }
+
+    public void MedicalOptions()
+    {
+        SceneManager.LoadScene(5);
+    }
+
+    public void back()
+    {
+        SceneManager.LoadScene(2);
+    }
+
+}
